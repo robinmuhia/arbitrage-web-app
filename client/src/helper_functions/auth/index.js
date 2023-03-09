f54f740b7b34992ffde5aa227eb03ec53a8b191c
robinmuhia <muhiarobinonyancha@gmail.com>
2023-03-09 08:29:29 +0300

    Fine-tuned Layout api to front-end

--- client/src/helper_functions/auth/index.js
+++ client/src/helper_functions/auth/index.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 
 export function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
-    console.log(user)
     if (user && user.access_token) {
         return `${user.token_type} ${user.access_token}`
     } else {
